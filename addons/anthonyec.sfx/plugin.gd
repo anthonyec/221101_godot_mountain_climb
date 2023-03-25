2b1acc4a1ad88a5e6ed41fe68889da060b6ff2e3
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-03-25 16:03:47 +0000

    Fix SFX plugin

--- addons/anthonyec.sfx/plugin.gd
+++ addons/anthonyec.sfx/plugin.gd
@@ -5,7 +5,11 @@ const SETTINGS_PROPERTY = "addons/sfx/sounds"
 
 func _enter_tree() -> void:
 	add_autoload_singleton("SFX", "res://addons/anthonyec.sfx/sfx.gd")
-	
+
+func _exit_tree() -> void:
+	remove_autoload_singleton("SFX")
+
+func _enable_plugin() -> void:
 	ProjectSettings.set_setting(SETTINGS_PROPERTY, "")
 	ProjectSettings.set_initial_value(SETTINGS_PROPERTY, "")
 	
@@ -15,9 +19,9 @@ func _enter_tree() -> void:
 		"hint": PROPERTY_HINT_DIR,
 		"hint_string": ""
 	})
-
-func _exit_tree() -> void:
-	remove_autoload_singleton("SFX")
 	
+	ProjectSettings.save()
+	
+func _disable_plugin() -> void:
 	if ProjectSettings.get_setting("addons/sfx/sounds", null):
 		ProjectSettings.clear(SETTINGS_PROPERTY)
